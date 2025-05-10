6604d3644e8f5be8bb6c6214617ac587dcfc6fb9
上條拓朗 <uejou@atotok.co.jp>
2025-05-10 12:22:12 +0900

    :tada: initial commit!!

---
+++ src/main/java/com/example/demo/TkBotApplication.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TkBotApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TkBotApplication.class, args);
+	}
+
+}
