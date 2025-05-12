0943771147736530db5d2448a9bcbb8af10a39a4
RentaroUejo <104684597+rentarouapp@users.noreply.github.com>
2025-05-12 17:40:29 +0900

    ルーティング追加 (#4)
    * :art: 依存関係を追加する
    
    * :art: HTMLとControllerを追加
    
    ---------
    
    Co-authored-by: uejo <rentarou1994@gmail.com>

---
+++ src/main/java/com/example/demo/PageController.java
@@ -0,0 +1,12 @@
+package com.example.demo;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class PageController {
+    @GetMapping("/test")
+    public String showTestPage() {
+        return "test";  // templates/test.html を探して返す
+    }
+}
