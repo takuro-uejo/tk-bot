e8b49a0d3ddb96be525c07760a9c9819b518e2bf
rentaro <rentarouapp@gmail.com>
2025-05-13 18:02:42 +0900

    :bug: パスをwebhookにしてLINEからのリクエストにとりあえず常に200を返しておく

--- src/main/java/com/example/demo/LineBotController.java
+++ src/main/java/com/example/demo/LineBotController.java
@@ -4,13 +4,12 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
-@RequestMapping("/linebot")
+@RequestMapping("/webhook")
 public class LineBotController {
     @PostMapping
     public ResponseEntity<String> webhook(@RequestBody String payload) {
         // LINEからのリクエスト（JSON）を受け取ってログに出力
         System.out.println("LINE Webhook 受信: " + payload);
-
         // 仮の応答（LINEの要件：200 OKを返す）
         return ResponseEntity.ok("OK");
     }
