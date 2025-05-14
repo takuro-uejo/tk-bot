f4ae50bc1e1a20c4e21433631f05a9b4714ab941
rentaro <rentarouapp@gmail.com>
2025-05-14 19:09:10 +0900

    :bug: モデルクラスがミスっていた

--- src/main/java/com/example/tkbot/model/LineWebhookRequest.java
+++ src/main/java/com/example/tkbot/model/LineWebhookRequest.java
@@ -1,28 +1,48 @@
 package com.example.tkbot.model;
 
-import java.util.List;
 import lombok.Getter;
 import lombok.Setter;
 
-public class LineWebhookRequest {
-    private List<Event> events;
+import java.util.List;
 
-    // Getter & Setter
-    public List<Event> getEvents() { return events; }
-    public void setEvents(List<Event> events) { this.events = events; }
+@Getter
+@Setter
+public class LineWebhookRequest {
+    private String destination;
+    private List<Event> events;
 
     @Getter
     @Setter
     public static class Event {
-        private String replyToken;
         private String type;
         private Message message;
+        private String webhookEventId;
+        private DeliveryContext deliveryContext;
+        private long timestamp;
+        private Source source;
+        private String replyToken;
+        private String mode;
     }
 
     @Getter
     @Setter
     public static class Message {
         private String type;
+        private String id;
+        private String quoteToken;
         private String text;
+    }
+
+    @Getter
+    @Setter
+    public static class DeliveryContext {
+        private boolean isRedelivery;
+    }
+
+    @Getter
+    @Setter
+    public static class Source {
+        private String type;
+        private String userId;
     }
 }
