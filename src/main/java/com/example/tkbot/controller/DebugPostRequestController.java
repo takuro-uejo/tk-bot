c0e497c08837288e306a432e71196cd165c31afa
takuro <rubyon.uejo9918@gmail.com>
2025-05-23 17:03:05 +0900

    前後3日の曜日を返す (#11)
    Co-authored-by: 上條拓朗 <rubyon.ruby@gmail.com>

--- src/main/java/com/example/tkbot/controller/DebugPostRequestController.java
+++ src/main/java/com/example/tkbot/controller/DebugPostRequestController.java
@@ -3,6 +3,11 @@ package com.example.tkbot.controller;
 import com.example.tkbot.model.DebugPostRequest;
 import com.fasterxml.jackson.databind.ObjectMapper;
 
+import java.time.DayOfWeek;
+import java.time.LocalDate;
+import java.time.format.TextStyle;
+import java.util.Locale;
+
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
@@ -18,7 +23,22 @@ public class DebugPostRequestController {
             DebugPostRequest request = objectMapper.readValue(payload, DebugPostRequest.class);
             String message = request.getTestmessage();
             System.out.println("DEBUG: 受け取ったメッセージ → " + message);
-            return ResponseEntity.ok("DEBUG: 受け取ったメッセージ → " + message);
+            LocalDate today;
+            String dayOfWeek;
+            // 昨日or今日or明日の判定
+            if (message.equals("昨日")) {
+                today = LocalDate.now().minusDays(1);
+                dayOfWeek = today.getDayOfWeek().getDisplayName(TextStyle.FULL, Locale.JAPAN);
+            } else if(message.equals("今日")){
+                today = LocalDate.now();
+                dayOfWeek = today.getDayOfWeek().getDisplayName(TextStyle.FULL, Locale.JAPAN);
+            } else if(message.equals("明日")){
+                today = LocalDate.now().plusDays(1);
+                dayOfWeek = today.getDayOfWeek().getDisplayName(TextStyle.FULL, Locale.JAPAN);
+            } else {
+                dayOfWeek = "そんなものはない";
+            }
+            return ResponseEntity.ok("DEBUG: 受け取ったメッセージ → " + dayOfWeek);
         } catch (Exception e) {
             e.printStackTrace();
             System.out.println("DEBUG: JSONパースに失敗しました: " + e.getMessage());
